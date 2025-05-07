c19989e04ca02f74b4c4fde144fb14b19c474586
Robert van den Boorn <robertb@uniform.dev>
2025-05-07 09:56:46 +0200

    feat: added uniform preview handler api route

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,6 @@
+import { createPreviewHandler } from "@uniformdev/canvas-next";
+
+// Preview Mode, more info https://nextjs.org/docs/advanced-features/preview-mode
+export default createPreviewHandler({
+  secret: () => process.env.UNIFORM_PREVIEW_SECRET || "norlys-preview-secret",
+});
